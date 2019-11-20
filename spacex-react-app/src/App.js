c1967b16de885218555a8ca4bdedc8bb52d533c4
Mike Cardarelli <Mike@Mikes-MacBook-Air.local>
2019-11-20 09:41:03 -0700

    registration route working react

--- spacex-react-app/src/App.js
+++ spacex-react-app/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Register from './Register';
+// import Login from './Login';
 import './App.css';
+// import CommentContainer from './CommentContainer';
+// import HeaderComponent from './HeaderComponent';
+import { Route, Switch } from 'react-router-dom';
+
+const My404 = () => {
+  return(
+    <div>
+      <h3>You have gotten lost.</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <main>
+      {/* <HeaderComponent /> */}
+      <Switch>
+        <Route exact path="/" component={Register} />
+        {/* <Route exact path="/login" component={Login} /> */}
+        {/* <Route exact path="/comments" component={CommentContainer} /> */}
+        <Route component={My404} />
+      </Switch>
+    </main>
   );
 }
 
